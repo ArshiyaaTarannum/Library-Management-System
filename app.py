5ed0f1f87f5100595c8d0f26d6d48865a87b6c00
ArshiyaaTarannum <arshiyatarannum824@gmail.com>
2026-08-06 22:42:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,56 @@ def library_rules():
         policy=policy
     )
 
+
+@app.route("/dashboard")
+def dashboard():
+
+    # ---------- Statistics ----------
+
+    cur.execute("SELECT COUNT(*) FROM Book")
+    total_books = cur.fetchone()[0]
+
+    cur.execute("SELECT COUNT(*) FROM BookCopy")
+    total_copies = cur.fetchone()[0]
+
+    cur.execute("""
+        SELECT COUNT(*)
+        FROM Member
+        WHERE IsActive = 1
+    """)
+    total_members = cur.fetchone()[0]
+
+    cur.execute("""
+        SELECT COUNT(*)
+        FROM IssueTransaction
+        WHERE Status = 'Issued'
+    """)
+    issued_books = cur.fetchone()[0]
+
+    cur.execute("""
+        SELECT COUNT(*)
+        FROM IssueTransaction
+        WHERE Status = 'Issued'
+        AND DueDate < CURDATE()
+    """)
+    overdue_books = cur.fetchone()[0]
+
+    cur.execute("""
+        SELECT COUNT(*)
+        FROM Shelf
+        WHERE Status = 'Active'
+    """)
+    total_shelves = cur.fetchone()[0]
+
+    return render_template(
+        "dashboard.html",
+        total_books=total_books,
+        total_copies=total_copies,
+        total_members=total_members,
+        issued_books=issued_books,
+        overdue_books=overdue_books,
+        total_shelves=total_shelves
+    )
 # ---------------- RUN FLASK ----------------
 
 
